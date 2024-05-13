2d0b18f3c4e047d6da05e3fd894de3269b820be7
raul <raulzambranoleon15@gmail.com>
2024-05-12 20:31:46 -0500

    eliminarDuplicados.py: mueve el archivo base a completos y genera el nuevo en la siguiente carpeta, solo genera una columna

--- eliminaDuplicados.py
+++ eliminaDuplicados.py
@@ -1,30 +1,47 @@
 import pandas as pd
+import os
+
 def main():
-    #encontrasmo la ruta actual
-    #ruta_actual = os.getcwd()
-    nombreArchivo=input('ingrese nombre del archivo: ')
+    # Obtener la ruta actual del directorio
+    ruta_actual = os.getcwd()
+    
+    # Solicitar al usuario el nombre del archivo de Excel
+    nombreArchivo = input('Ingrese nombre del archivo (sin la extension .xlsx): ')
+    
+    # Leer el archivo Excel
     df = pd.read_excel(f'{nombreArchivo}.xlsx')
-    print(f'numero de filas inicial: {df.shape[0]}')
-    nfInicio=df.shape[0]
+    print(f'Numero de filas inicial: {df.shape[0]}')
+    nfInicio = df.shape[0]
 
-    # Suponiendo que tienes un DataFrame llamado df y deseas eliminar duplicados basados en la columna 'ruc'
-    df_sin_duplicados = df.drop_duplicates(subset=['ruc'], keep='first')
+    # Convertir nombres de columnas a minúsculas y seleccionar solo la columna 'ruc'
+    df.columns = map(str.lower, df.columns)  # Convertir nombres de columnas a minúsculas
+    df_ruc = df[['ruc']]  # Seleccionar solo la columna 'ruc'
 
-    print(f'numero de filas final: {df_sin_duplicados.shape[0]}')
-    nfFin=df_sin_duplicados.shape[0]
-    # Abrir un archivo de texto en modo escritura
+    # Eliminar duplicados basados en la columna 'ruc'
+    df_sin_duplicados = df_ruc.drop_duplicates(keep='first')
+
+    print(f'Numero de filas final: {df_sin_duplicados.shape[0]}')
+    nfFin = df_sin_duplicados.shape[0]
+    
+    # Escribir en un archivo de texto
     with open(f'{nombreArchivo}.txt', 'w') as archivo:
-        # Escribir contenido en el archivo
         archivo.write(f"Nombre del archivo: {nombreArchivo}.\n")
-        archivo.write(f"numero de items inicio: {nfInicio}.\n")
-        archivo.write(f"numero de items al finalizar: {nfFin}.\n")
-        archivo.write(f"duplicados: {nfInicio-nfFin}.\n")
+        archivo.write(f"Numero de items al inicio: {nfInicio}.\n")
+        archivo.write(f"Numero de items al finalizar: {nfFin}.\n")
+        archivo.write(f"Duplicados encontrados: {nfInicio - nfFin}.\n")
     print("Archivo creado satisfactoriamente.")
     
+    # Mover el archivo Excel a la carpeta "completados"
+    if not os.path.exists('completados'):
+        os.makedirs('completados')
+    os.rename(f'{nombreArchivo}.xlsx', f'completados/{nombreArchivo}.xlsx')
     
-    nombreArchivoSalida=input('ingrese el nombre del archico de salida: ')
+    # Crear la ruta de la siguiente carpeta para el proceso
+    ruta_creacion = ruta_actual.replace("1.BDBruto", "2.BDNeto\\")
     
-    df_sin_duplicados.to_excel(f'{nombreArchivoSalida}.xlsx',index=False)
-    
-if __name__== '__main__':
+    # Guardar solo la columna 'ruc' en un nuevo archivo Excel y moverlo a la siguiente carpeta
+    df_sin_duplicados.to_excel(f'{ruta_creacion}{nombreArchivo}.xlsx', index=False)
+    print("Se creó el archivo en la carpeta siguiente, con el mismo nombre")
+
+if __name__ == '__main__':
     main()
